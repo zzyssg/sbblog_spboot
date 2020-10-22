6726585238150da850becc081eba206175d23c6e
zzyssg <zy1704528@buaa.edu.cn>
2020-10-22 16:49:25 +0800

    解决添加重复问题

--- src/main/java/com/zzy/sbblog/controller/BlogController.java
+++ src/main/java/com/zzy/sbblog/controller/BlogController.java
@@ -2,11 +2,16 @@ package com.zzy.sbblog.controller;
 
 import com.zzy.sbblog.entity.Blog;
 import com.zzy.sbblog.entity.ResponseBean;
+import com.zzy.sbblog.entity.Type;
+import com.zzy.sbblog.exception.CommonException;
 import com.zzy.sbblog.service.BlogService;
+import com.zzy.sbblog.service.TypeService;
 import com.zzy.sbblog.vo.BlogArchieveVO;
 import com.zzy.sbblog.vo.BlogVO;
+import com.zzy.sbblog.vo.TypesAndTagsVo;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +20,9 @@ import java.util.List;
 public class BlogController {
     @Resource
     private BlogService blogService;
+
+    @Resource
+    private TypeService typeService;
 
     @PostMapping("/getAllBlogVOs")
     public ResponseBean getAllBlogVOs() {
@@ -93,6 +101,24 @@ public class BlogController {
             responseBean = new ResponseBean("001", "success", blog);
         }
         return responseBean;
+    }
+
+    @GetMapping("/findAllTypesAndTags")
+    public ResponseBean findAllTypesAndTags(){
+        ResponseBean responseBean;
+        /*
+        * 1、调用typeMapper，查询types
+        * 2、调用tagsMapper，查询tags
+        * 3、ArrayList，放入types、tags，
+        * */
+        List<Type> typeList = typeService.queryAllTypes();
+        if(typeList == null){
+            throw new CommonException("000","没有[类别]或者[类别]错误！");
+        }
+        TypesAndTagsVo typesAndTags = new TypesAndTagsVo(typeList);
+        responseBean = new ResponseBean("001","success",typesAndTags);
+        return responseBean;
+
 
 
     }
