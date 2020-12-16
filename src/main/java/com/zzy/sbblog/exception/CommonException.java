89b56d580dbfa03d1afdf4498959e4295f61ac6a
zzyssg <zy1704528@buaa.edu.cn>
2020-12-16 20:21:28 +0800

    add some @Data

--- src/main/java/com/zzy/sbblog/exception/CommonException.java
+++ src/main/java/com/zzy/sbblog/exception/CommonException.java
@@ -1,53 +1,21 @@
 package com.zzy.sbblog.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author zzy
  * @Date 2020/10/27 17:33
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommonException extends RuntimeException {
 
     private String retCode;
     private String retMsg;
 
-    /**
-     * Constructs a new runtime exception with {@code null} as its
-     * detail message.  The cause is not initialized, and may subsequently be
-     * initialized by a call to {@link #initCause}.
-     */
-    public CommonException() {
-    }
-
-    /**
-     * Constructs a new runtime exception with {@code null} as its
-     * detail message.  The cause is not initialized, and may subsequently be
-     * initialized by a call to {@link #initCause}.
-     */
-    public CommonException(String retCode, String retMsg) {
-        this.retCode = retCode;
-        this.retMsg = retMsg;
-    }
-
-    public String getRetCode() {
-        return retCode;
-    }
-
-    public String getRetMsg() {
-        return retMsg;
-    }
-
-    public void setRetCode(String retCode) {
-        this.retCode = retCode;
-    }
-
-    public void setRetMsg(String retMsg) {
-        this.retMsg = retMsg;
-    }
-
-    @Override
-    public String toString() {
-        return "CommonException{" +
-                "retCode='" + retCode + '\'' +
-                ", retMsg='" + retMsg + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/zzy/sbblog/vo/TypesAndTagsVo.java
+++ src/main/java/com/zzy/sbblog/vo/TypesAndTagsVo.java
@@ -1,6 +1,10 @@
 package com.zzy.sbblog.vo;
 
 import com.zzy.sbblog.entity.Type;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -8,30 +12,12 @@ import java.util.List;
  * @author zzy
  * @Date 2020/10/27 17:33
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TypesAndTagsVo {
     private List<Type> types;
 //    private List<Tags> tags;
 
-
-    public TypesAndTagsVo() {
-    }
-
-    public TypesAndTagsVo(List<Type> types) {
-        this.types = types;
-    }
-
-    public List<Type> getTypes() {
-        return types;
-    }
-
-    public void setTypes(List<Type> types) {
-        this.types = types;
-    }
-
-    @Override
-    public String toString() {
-        return "TypesAndTagsVo{" +
-                "types=" + types +
-                '}';
-    }
 }
